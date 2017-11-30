3cf4bad9d76df58c2935fa91f3be7c4304cb3aaa
asorathiya <sorathiya.ankit@gmail.com>
2017-11-30 11:21:07 +0530

    adding maximum triangle sum

---
+++ src/main/java/com/projecteuler/math/triangle/MaxtimumTrianglePathSum.java
@@ -0,0 +1,105 @@
+package com.projecteuler.math.triangle;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.stream.Stream;
+
+public class MaxtimumTrianglePathSum {
+	public static int findMaximumTriangleSumPath(String[] triangle) {
+		TriangleNode root = buildTriangleTree(triangle);
+		int[] maxSum = new int[1];
+		findMaximumTriangleSumPath(root, 0, maxSum);
+		return maxSum[0];
+	}
+
+	private static void findMaximumTriangleSumPath(TriangleNode root, int currentSum, int[] maxSum) {
+		if (root == null) {
+			if (maxSum[0] < currentSum) {
+				maxSum[0] = currentSum;
+			}
+			return;
+		}
+		currentSum += root.getValue();
+		findMaximumTriangleSumPath(root.getLeftChild(), currentSum, maxSum);
+		findMaximumTriangleSumPath(root.getRightChild(), currentSum, maxSum);
+	}
+
+	private static TriangleNode buildTriangleTree(String[] triangle) {
+		if (triangle == null || triangle.length == 0) {
+			return new TriangleNode(-1);
+		}
+		int rootValue = Integer.parseInt(triangle[0]);
+		TriangleNode root = new TriangleNode(rootValue);
+		Queue<TriangleNode> parents = new LinkedList<>();
+		parents.add(root);
+		for (int index = 1; index < triangle.length; index++) {
+			int[] currentDepthNodes = convertToInts(triangle[index].split(" "));
+			Queue<TriangleNode> goingToBeParents = new LinkedList<>();
+			TriangleNode commonChild = null;
+			int nodeIndex = 0;
+			while (!parents.isEmpty() && nodeIndex < currentDepthNodes.length) {
+				TriangleNode node = parents.poll();
+				TriangleNode leftChild = null;
+				if (commonChild != null) {
+					leftChild = commonChild;
+				} else {
+					leftChild = new TriangleNode(currentDepthNodes[nodeIndex]);
+					goingToBeParents.add(leftChild);
+				}
+				TriangleNode rightChild = new TriangleNode(currentDepthNodes[nodeIndex + 1]);
+				node.setLeftChild(leftChild);
+				node.setRightChild(rightChild);
+				commonChild = rightChild;
+				goingToBeParents.add(rightChild);
+				nodeIndex++;
+			}
+			parents = goingToBeParents;
+		}
+
+		return root;
+	}
+
+	private static int[] convertToInts(String[] values) {
+		return Stream.of(values).mapToInt(Integer::parseInt).toArray();
+	}
+}
+
+class TriangleNode {
+	private int value;
+	private TriangleNode leftChild;
+	private TriangleNode rightChild;
+
+	public TriangleNode(int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public TriangleNode getLeftChild() {
+		return leftChild;
+	}
+
+	public TriangleNode getRightChild() {
+		return rightChild;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public void setLeftChild(TriangleNode leftChild) {
+		this.leftChild = leftChild;
+	}
+
+	public void setRightChild(TriangleNode rightChild) {
+		this.rightChild = rightChild;
+	}
+
+	@Override
+	public String toString() {
+		return String.valueOf(value);
+	}
+
+}
---
+++ src/test/java/com/projecteuler/math/triangle/MaxtimumTrianglePathSumTest.java
@@ -0,0 +1,26 @@
+package com.projecteuler.math.triangle;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MaxtimumTrianglePathSumTest {
+	@Test
+	public void testMaxTrianglePathWithSmallData() {
+		String[] triangle = new String[] { "3", "7 4", "2 4 6", "8 5 9 3" };
+		int maxPathSum = MaxtimumTrianglePathSum.findMaximumTriangleSumPath(triangle);
+		assertEquals(23, maxPathSum);
+	}
+
+	@Test
+	public void testMaxTrianglePathWithBigData() {
+
+		String[] triangle = new String[] { "75", "95 64", "17 47 82", "18 35 87 10", "20 04 82 47 65",
+				"19 01 23 75 03 34", "88 02 77 73 07 63 67", "99 65 04 28 06 16 70 92", "41 41 26 56 83 40 80 70 33",
+				"41 48 72 33 47 32 37 16 94 29", "53 71 44 65 25 43 91 52 97 51 14",
+				"70 11 33 28 77 73 17 78 39 68 17 57", "91 71 52 38 17 14 91 43 58 50 27 29 48",
+				"63 66 04 68 89 53 67 30 73 16 69 87 40 31", "04 62 98 27 23 09 70 98 73 93 38 53 60 04 23" };
+		int maxPathSum = MaxtimumTrianglePathSum.findMaximumTriangleSumPath(triangle);
+		assertEquals(1074, maxPathSum);
+	}
+}
